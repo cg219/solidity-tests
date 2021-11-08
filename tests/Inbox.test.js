93879d4122af6fc871f3acf4a93de1ba91bf2c5a
Clemente <dev@imkreative.com>
2021-11-07 23:49:50 -0500

    Add tests for updating contract

--- tests/Inbox.test.js
+++ tests/Inbox.test.js
@@ -1,5 +1,6 @@
 import { resolve } from 'https://deno.land/std@0.113.0/path/mod.ts';
 import { filterValues } from 'https://deno.land/std@0.113.0/collections/mod.ts';
+import { assert, assertEquals } from 'https://deno.land/std@0.113.0/testing/asserts.ts';
 import Web3 from 'https://deno.land/x/web3@v0.4.3/mod.ts'
 
 const web3 = new Web3('ws://localhost:8545');
@@ -13,12 +14,34 @@ const abi = decoder.decode(abiData);
 let accounts;
 let inbox;
 
-Deno.test('Testing Web3', async () => {
+Deno.test('Deploy Contract 1', async () => {
+    accounts = await web3.eth.getAccounts();
+
+    await deployContract(accounts[0]);
+    assert(inbox.options.address);
+})
+
+Deno.test('It has a default message', async () => {
     accounts = await web3.eth.getAccounts();
 
     await deployContract(accounts[0]);
 
-    console.log(inbox);
+    const message = await inbox.methods.message().call();
+
+    assertEquals(message, 'Its Mente from Ethereum');
+})
+
+Deno.test('It can update message', async () => {
+    accounts = await web3.eth.getAccounts();
+
+    await deployContract(accounts[0]);
+    await inbox.methods.setMessage('Its Mente from the Test').send({
+        from: accounts[0]
+    });
+
+    const message = await inbox.methods.message().call();
+
+    assertEquals(message, 'Its Mente from the Test');
 })
 
 async function deployContract(account) {
